59edb092358e459feb3f88c66b9369da4743f886
Wilie Wahyu Hidayat <wilie.hidayat87@gmail.com>
2023-03-07 20:15:13 +0700

    add func shell exec

--- util.go
+++ util.go
@@ -558,3 +558,13 @@ func Copy(srcFolder string, destFolder string) bool {
 		return false
 	}
 }
+
+func Shellout(command string) (string, string, error) {
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd := exec.Command("bash", "-c", command)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	return stdout.String(), stderr.String(), err
+}
